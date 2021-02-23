0b552e074ecebadee6634d10d0b70cd028b5f306
Rodrigo D <rodrigodalfre@gmail.com>
2021-02-23 18:11:25 -0300

    button dark mode and 'menu'

---
+++ index.php
@@ -0,0 +1,134 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="icon" type="image/png" sizes="32x32" href="./images/favicon-32x32.png">
+    <title>Document</title>
+
+    <!-- Style -->
+    <link rel="stylesheet" href="css/style.css">
+
+    <!-- Javascript -->
+    <script src="js/script.js"></script>
+
+
+</head>
+<body>
+    
+
+		<header>
+			<nav id="menu">
+				<div class="container">
+					<ul>
+						<li class="item">
+							<span>
+								Social Media Dashboard
+							</span>
+							<br>
+							<span>
+								Total Followers: 23,004
+							</span>
+						</li>
+						<li class="darkmode">
+								<span class="darkmode-span">Dark Mode</span>
+								<input id="night-mode" class="switch night--mode" type="checkbox">
+								<label for="night-mode"></label>
+						</li>
+					</ul>
+				</div>
+			</nav>
+		</header>
+
+		<div class="container">
+
+		<div class="cards">
+				<div>
+					<div>
+							@Elizabeto
+							1987
+							Followers
+							12 Today
+					</div>
+				</div>
+
+				<div class="card">
+					<div>
+							@Elizabeto
+							1044
+							Followers
+							99 Today
+					</div>
+				</div>
+
+				<div class="card">
+					<div>
+							@Elizabeto
+							11k
+							Followers
+							1099 Today
+					</div>
+				</div>
+
+				<div class="card">
+					<div>
+							Rodrigo D.
+							8239
+							Subscribers
+							144 Today
+					</div>
+			</div>
+
+
+		</div>
+
+	
+
+
+	
+
+	
+
+	
+
+
+
+	Overview - Today
+
+	Page Views
+	87
+	3%
+
+	Likes
+	52
+	2%
+
+	Likes
+	5462
+	2257%
+
+	Profile Views
+	52k
+	1375%
+
+	Retweets
+	117
+	303%
+
+	Likes
+	507
+	553%
+
+	Likes
+	107
+	19%
+
+	Total Views
+	1407
+	12%
+
+  </div>
+
+</body>
+</html>
